35bbb042205bf143af3122aebd5a2883f22c815f
Daniel Gil <tiverius37@gmail.com>
2023-08-18 02:02:11 -0500

    Added router implementation with path to login and main dashboard ("/"), set it up to be used with HTML5 standards. added guard beforeach for redirecting unautheneticated users to /login.

--- vue_winged/src/main.js
+++ vue_winged/src/main.js
@@ -1,5 +1,35 @@
-import { createApp } from 'vue'
 import App from './App.vue'
+import { createApp } from 'vue'
 import axiosInstance from './axiosInstance'
+import axiosAuthentication from './axiosAuthentication'
+import { createRouter, createWebHistory } from 'vue-router';
+import LoginComponent from './components/LoginComponent.vue';
+import DashboardComponent from './components/DashboardComponent.vue';
 
-createApp(App).mount('#app')
+///axiosAuthentication.login("flac","pepito56")
+
+const routes = [
+  { path: '/login', component: LoginComponent },
+  { path: '/', component: DashboardComponent, meta: { requiresAuth: true } },
+];
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+
+router.beforeEach((to, from, next) => {
+  const isAuthenticated = localStorage.getItem('token');
+
+  if (to.meta.requiresAuth && !isAuthenticated) {
+    next('/login');
+  } else {
+    next(); // Change this to next() to allow navigation to the original target route
+  }
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
